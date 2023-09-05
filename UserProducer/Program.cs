be4cc559638ffd9eb3ed7b86d186500da472aa0d
Emanuel Jimenez <emanuel.jimenez+DFIN@dfinsolutions.com>
2023-09-05 15:28:43 -0300

    fist commit

---
+++ UserProducer/Program.cs
@@ -0,0 +1,27 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+
+var config = new ProducerConfig { BootstrapServers = "0.0.0.0:9092" };
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+try
+{
+	string? name;
+	while ((name = Console.ReadLine()) != null)
+	{
+		Random rnd = new Random();
+        var response = await producer.ProduceAsync("userspermissions-topic",
+        new Message<Null, string> { Value = JsonConvert.SerializeObject(
+		new User(name, rnd.Next()))});
+		Console.WriteLine(response.Value);
+    }	
+
+}
+catch (Exception ex)
+{
+
+	throw;
+}
+
+public record User (string name, int Guid);
+
